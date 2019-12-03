9264c83a7bb1c5a0ef2116a68d218439c33058d5
Mi&Jack <mijackstudio@gmail.com>
2019-12-03 23:24:54 +0800

    init project

---
+++ src/main/java/com/mijack/cangjie/CangjieApplication.java
@@ -0,0 +1,13 @@
+package com.mijack.cangjie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CangjieApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CangjieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mijack/cangjie/CangjieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mijack.cangjie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CangjieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
