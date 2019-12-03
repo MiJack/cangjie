f426b782f9a6715ac553f7c1d57ad299c0cb0455
Mi&Jack <mijackstudio@gmail.com>
2019-12-03 23:59:05 +0800

    add spring

--- src/main/java/com/mijack/cangjie/CangjieApplication.java
+++ src/main/java/com/mijack/cangjie/CangjieApplication.java
@@ -1,13 +1,45 @@
+/*
+ *    Copyright 2019 Mi&Jack
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
 package com.mijack.cangjie;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.StandardEnvironment;
 
+/**
+ * @author Mi&Jack
+ */
 @SpringBootApplication
 public class CangjieApplication implements CommandLineRunner {
+public static final Logger logger = LoggerFactory.getLogger(CangjieApplication.class);
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplicationBuilder(CangjieApplication.class)
+                .environment(new StandardEnvironment())
+                .application();
+        application.run(args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CangjieApplication.class, args);
-	}
-
+    @Override
+    public void run(String... args) {
+        logger.info("run info: args = {}", args);
+    }
 }
